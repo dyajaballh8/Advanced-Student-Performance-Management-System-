c1578b2c7ee41d37375fccd896bc371310290cd4
dyajaballh8 <dyajaballh8@gmail.com>
2025-12-18 22:51:34 +0200

    Update DBConnection.java

--- SRC/DBConnection.java
+++ SRC/DBConnection.java
@@ -1,15 +1,8 @@
 package ada.projectfinal;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
+import java.sql.*;
 public class DBConnection {
-
-    private static final String URL =
-        "jdbc:sqlserver://localhost:1433;databaseName=StudentPerformanceDB;encrypt=true;trustServerCertificate=true;integratedSecurity=true";
-
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL);
+        String url = "jdbc:sqlserver://localhost:1433;databaseName=StudentPerformanceDB;encrypt=true;trustServerCertificate=true;integratedSecurity=true";
+        return DriverManager.getConnection(url);
     }
 }
