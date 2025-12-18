810b9da0b5daa04aa6899a7d4a6ff0c3a081b7b6
dyajaballh8 <dyajaballh8@gmail.com>
2025-12-18 22:54:01 +0200

    Update FacultyDashboard.java

--- GUI/FacultyDashboard.java
+++ GUI/FacultyDashboard.java
@@ -1,1 +1,9 @@
-
+package ada.projectfinal;
+import javax.swing.*;
+public class FacultyDashboard extends JFrame {
+    public FacultyDashboard() {
+        setTitle("Faculty Portal"); setSize(400, 300);
+        add(new JLabel("Welcome Faculty Member", SwingConstants.CENTER));
+        setLocationRelativeTo(null);
+    }
+}
