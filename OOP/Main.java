754f958b317af3d2224a2cb15dedcf89b954b2c8
dyajaballh8 <dyajaballh8@gmail.com>
2025-12-18 22:50:50 +0200

    Update and rename Main to Main.java

---
+++ OOP/Main.java
@@ -0,0 +1,6 @@
+package ada.projectfinal;
+public class Main {
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> { new LoginFrame().setVisible(true); });
+    }
+}
