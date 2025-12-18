eb9dbc6528f043c5860422e1f35c01bd117a2ffb
dyajaballh8 <dyajaballh8@gmail.com>
2025-12-18 22:54:22 +0200

    Update StudentDashboard.java

--- GUI/StudentDashboard.java
+++ GUI/StudentDashboard.java
@@ -1,1 +1,16 @@
-
+package ada.projectfinal;
+import javax.swing.*;
+import java.awt.*;
+public class StudentDashboard extends JFrame {
+    public StudentDashboard(Student s) {
+        setTitle("Student Dashboard"); setSize(500, 400); setLayout(new GridLayout(6, 1));
+        add(new JLabel("Welcome: " + s.getName(), SwingConstants.CENTER));
+        add(new JLabel("Major: " + s.getMajor(), SwingConstants.CENTER));
+        add(new JLabel("GPA: " + s.getGpa(), SwingConstants.CENTER));
+        JLabel alert = new JLabel("", SwingConstants.CENTER);
+        new AlertManager().updateAlertLabel(alert, new AttendanceDAO().getAttendedDays(s.getUserId()));
+        add(alert);
+        JButton logout = new JButton("Logout"); logout.addActionListener(e -> { new LoginFrame().setVisible(true); dispose(); });
+        add(logout); setLocationRelativeTo(null);
+    }
+}
